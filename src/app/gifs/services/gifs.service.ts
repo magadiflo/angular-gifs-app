0c90a442e61fee5be09df3d7694173bc5037cf5e
Martín <magadiflo@gmail.com>
2022-07-14 22:28:24 -0500

    Realizar una petición HTTP

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,17 +1,20 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GifsService {
 
+  private readonly apiKey: string = '3ZFhGljhvA8ctX3W29sNy5xCrjAqBwLE';
+  private readonly apiGif: string = 'http://api.giphy.com/v1/gifs/search?api_key=3ZFhGljhvA8ctX3W29sNy5xCrjAqBwLE&q=goku&limit=10';
   private _historial: string[] = [];
 
   get historial(): string[] {
     return [...this._historial];
   }
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   buscarGifs(query: string = ''): void {
     query = query.toLowerCase();
@@ -19,6 +22,12 @@ export class GifsService {
       this._historial.unshift(query);
       this._historial = this._historial.splice(0, 10);
     }
+
+    this.http.get(`${this.apiGif}`)
+      .subscribe((resp: any) => {
+        console.log(resp.data);
+      });
+
   }
 
 
