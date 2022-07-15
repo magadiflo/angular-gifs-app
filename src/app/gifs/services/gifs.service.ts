ee67b993e2924ed685038661b3c41c87d8d6dda1
Martín <magadiflo@gmail.com>
2022-07-14 23:32:31 -0500

    Cargar imágenes automáticamente

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -19,6 +19,7 @@ export class GifsService {
 
   constructor(private http: HttpClient) {
     this._historial = JSON.parse(localStorage.getItem('historial')!) || [];
+    this.resultados = JSON.parse(localStorage.getItem('resultados')!) || [];
   }
 
   buscarGifs(query: string = ''): void {
@@ -35,6 +36,9 @@ export class GifsService {
       .subscribe(resp => {
         console.log(resp.data);
         this.resultados = resp.data;
+
+        //* Guardando el último resultado de búsqueda
+        localStorage.setItem('resultados', JSON.stringify(this.resultados));
       });
 
   }
